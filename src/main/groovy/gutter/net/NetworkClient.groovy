984602cb5fdf576db7a90871a9be36ca871a91ac
Kenneth Endfinger <kaendfinger@gmail.com>
2014-04-15 11:43:35 -0400

    Make Disposable @CompileStatic, add a WIP NetworkClient (Probably doesn't work)

---
+++ src/main/groovy/gutter/net/NetworkClient.groovy
@@ -0,0 +1,52 @@
+package gutter.net
+
+import groovy.transform.CompileStatic
+
+import java.nio.ByteBuffer
+import java.nio.channels.SocketChannel
+
+@CompileStatic
+class NetworkClient {
+    final InetSocketAddress address
+    private SocketChannel channel
+
+    NetworkClient(String host, int port) {
+        address = new InetSocketAddress(host, port)
+        channel = new Socket().channel
+    }
+
+    SocketChannel getChannel() {
+        channel
+    }
+
+    void connect() {
+        if (channel.connected) {
+            throw new IllegalStateException("Socket already connected!")
+        }
+        channel.connect(address)
+    }
+
+    void write(byte[] bytes) {
+        channel.write(ByteBuffer.wrap(bytes))
+    }
+
+    void writeln(String line) {
+        channel.write(ByteBuffer.wrap("${line}\n".bytes))
+    }
+
+    void write(String string) {
+        channel.write(ByteBuffer.wrap(string.bytes))
+    }
+
+    String readLine() {
+        String line = ""
+        while (true) {
+            def buffer = ByteBuffer.allocate(1)
+            def amount = channel.read(buffer).intValue()
+            if (amount == 0) {
+                break
+            }
+            line += buffer.getChar(0)
+        }
+    }
+}
--- src/main/groovy/gutter/util/Disposable.groovy
+++ src/main/groovy/gutter/util/Disposable.groovy
@@ -1,9 +1,12 @@
 package gutter.util
 
+import groovy.transform.CompileStatic
+
 /**
  * Gives a way to dispose of an object
  * <p>Note: This does not GC it, but it can be if necessary</p>
  */
+@CompileStatic
 interface Disposable {
     /**
      * Disposes the Object
